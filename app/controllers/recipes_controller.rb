3b0cd46891bbc93d25a03e35ede35f3135582b12
D1353L <meghanik400@gmail.com>
2017-02-15 14:20:17 +0200

    Initialize @recipes to empty array if request is nil has been added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
     request = params.include?(:search) ? params[:search] : 'chocolate'
-    @recipes = Recipe.for(request)
+    @recipes = Recipe.for(request) || []
   end
 end
